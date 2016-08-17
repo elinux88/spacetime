b122fcd73c1ac0f90a546d9da1e7eb2684627067
Eli Cook <elinux88@gmail.com>
2016-08-17 17:03:26 -0600

    Modified layout of search term dialog

--- app/src/main/java/site/elioplasma/ecook/spacetimeeventreminder/EditSearchTermFragment.java
+++ app/src/main/java/site/elioplasma/ecook/spacetimeeventreminder/EditSearchTermFragment.java
@@ -9,10 +9,13 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.text.InputType;
+import android.view.Gravity;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 /**
  * Created by eli on 4/7/16.
+ * Updated by eli on 8/17/16.
  */
 public class EditSearchTermFragment extends DialogFragment {
 
@@ -20,8 +23,6 @@ public class EditSearchTermFragment extends DialogFragment {
             "site.elioplasma.ecook.spacetimeeventreminder.searchterm";
 
     private static final String ARG_SEARCH_TERM = "search_term";
-
-    private EditText mEditText;
 
     public static EditSearchTermFragment newInstance(String searchTerm) {
         Bundle args = new Bundle();
@@ -37,18 +38,24 @@ public class EditSearchTermFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         String searchTerm = getArguments().getString(ARG_SEARCH_TERM);
 
-        mEditText = new EditText(getActivity());
-        mEditText.setInputType(InputType.TYPE_CLASS_TEXT);
-        mEditText.setText(searchTerm);
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setGravity(Gravity.CENTER_HORIZONTAL);
+        final EditText editText = new EditText(getActivity());
+        editText.setSingleLine(true);
+        editText.setInputType(InputType.TYPE_CLASS_TEXT);
+        layout.setPadding(40, 0, 40, 0);
+        editText.setText(searchTerm);
+        layout.addView(editText);
 
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.search_term_for_sky_map)
-                .setView(mEditText)
+                .setView(layout)
                 .setPositiveButton(android.R.string.ok,
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                sendResult(Activity.RESULT_OK, mEditText.getText().toString());
+                                sendResult(Activity.RESULT_OK, editText.getText().toString());
                             }
                         })
                 .create();
